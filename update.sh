3024af90db74723b221c8285fe890de8a03f92e1
Kristian <kristiantanuwijaya@staff.ubaya.ac.id>
2021-08-05 11:08:34 +0700

    add version update/upgrade script (will not automatically check for update, it is only to be run manually by user)

---
+++ update.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+
+OPTIND=1 # Reset in case getopts has been used previously in the shell.
+
+# boilerplate
+set -o errexit # exit when any command return non-zero exit code
+set -o nounset # exit when using undeclared variables
+exit_on_error() {
+    if test $# -eq 1; then
+        echo ">>> $1"
+    fi
+    exit 1
+}
+
+install_directory_target=""
+
+if test $# -eq 0; then
+    install_directory_target="/bin"
+else
+    install_directory_target="$1"
+fi
+
+install_directory_target=`readlink -f "$install_directory_target"`
+
+echo ">>> uninstalling from $install_directory_target"
+
+rm "$install_directory_target/git-worktree-create.sh" || exit_on_error "cannot remove git-worktree-create.sh, is it already installed?"
+rm "$install_directory_target/git-worktree-create" || exit_on_error "cannot remove git-worktree-create, is it already installed?"
+
+echo ">>> reinstalling to $install_directory_target"
+
+cp "git-worktree-create.sh" "$install_directory_target/git-worktree-create.sh" || exit_on_error "cannot copy git-worktree-create.sh, is it already installed?"
+ln "$install_directory_target/git-worktree-create.sh" "$install_directory_target/git-worktree-create" || exit_on_error "cannot copy git-worktree-create, is it already installed?"
+
+chmod u+x,g+x,o+x "$install_directory_target/git-worktree-create.sh"
+chmod u+x,g+x,o+x "$install_directory_target/git-worktree-create"
+
